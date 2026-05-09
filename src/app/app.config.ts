03a62c491d4a799263647231b12c217554269499
William Newman <38147848+william-newman@users.noreply.github.com>
2026-05-09 13:56:00 -0600

    security updates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { prodEnvironment } from '../environments/environment-prod'
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
